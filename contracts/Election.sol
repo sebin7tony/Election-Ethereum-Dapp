47b07d9bba4480e5d210d97a9cca4eafd1165b82
Sebin Tony <sebin7tony@gmail.com>
2018-09-21 17:40:53 -0400

    comments added

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,8 +2,13 @@ pragma solidity ^0.4.24;
 
 contract Election{
 	
+	//Stores the candidate value
+	//Since we use public it will give default getter method \
+	//to access this field in the truffle console.
+	//Access like app.candidate() 
 	string public candidate;
 
+	//It is the constructor
 	function Election() public {
 
 		candidate = "Candidate1";
