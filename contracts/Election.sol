e7ebe921d2ce4c535967c30a06c0805497ff27b2
Sebin Tony <sebin7tony@gmail.com>
2018-09-21 18:23:39 -0400

    candidate addition to the smart contract

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,15 +2,41 @@ pragma solidity ^0.4.24;
 
 contract Election{
 	
-	//Stores the candidate value
-	//Since we use public it will give default getter method \
-	//to access this field in the truffle console.
-	//Access like app.candidate() 
-	string public candidate;
+	//Model a candidate. Struct is used to create models
+	struct Candidate{
+		uint id;
+		string name;
+		uint voteCount;
+	}
 
-	//It is the constructor
-	function Election() public {
+	//Store candidates
+	//Fetch candidate
+	//mapping is kind of hashmap
+	mapping(uint=>Candidate) public candidates;
 
-		candidate = "Candidate1";
+	//Candidate count
+	uint public candidateCount;
+
+	constructor() public {
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+	}
+
+	//_name represent the local variable
+	//made private to restrict usage from outside
+	function addCandidate(string _name) private{
+		candidateCount++;
+		candidates[candidateCount] = Candidate(candidateCount,_name,0);
 	}
 }
+
+// Some truffle console hacks
+// 
+//  initialize the app by Election.deployed().then(function(instance){app =instance;})	 
+//
+//  get the candidate by app.candidates(1).then(function(c){candidate =c;});
+//
+// candidates returns BigNumber,string, BigNumber
+// get value from BigNumber by candidate[2].toNumber() 
+// 
+
